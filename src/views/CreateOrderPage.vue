039a6377bb2041425add07cb6d3807abea15f230
Inna Magkeeva <inna.magkeeva79@gmail.com>
2026-01-18 03:11:00 +0300

    валидация

--- src/views/CreateOrderPage.vue
+++ src/views/CreateOrderPage.vue
@@ -18,6 +18,51 @@ const orderForm = reactive({
   status: '' as OrderStatus | '',
 })
 
+// Создаем отдельный объект errors/ с теми же полями, что и в orderForm. !! тут НЕ дублируется ничего(orderForm и errors),одинаковые ключи но разный смысл,разный жизненный цикл
+
+const errors = reactive({
+  userName: '',
+  address: '',
+  date: '',
+  comment: '',
+  product: '',
+  status: '',
+})
+
+// Функция валидации через "руками"
+
+// function validateForm() {
+//   errors.userName = orderForm.userName ? '' : 'Заполните это поле'
+//   errors.address = orderForm.address ? '' : 'Заполните это поле'
+//   errors.date = orderForm.date ? '' : 'Заполните это поле'
+//   errors.comment = orderForm.comment ? '' : 'Заполните это поле'
+//   errors.product = orderForm.product ? '' : 'Заполните это поле'
+//   errors.status = orderForm.status ? '' : 'Заполните это поле'
+
+//   return (
+//     !errors.userName &&
+//     !errors.address &&
+//     !errors.date &&
+//     !errors.comment &&
+//     !errors.product &&
+//     !errors.status
+//   )
+// }Оставила для наглядности.закомментировала.
+
+// Функция валидации через перебор. Не руками по каждому полю,как закомментированный пример выше, а один цикл.
+function validateForm() {
+  let isValid = true
+
+  for (const key of Object.keys(errors) as Array<keyof typeof orderForm>) {
+    // тут говорится:«key — это НЕ любая строка,а один из ключей формы»
+    if (!orderForm[key]) {
+      errors[key] = 'Заполните это поле'
+      isValid = false
+    }
+  }
+  return isValid
+}
+
 function resetForm() {
   orderForm.address = ''
   orderForm.comment = ''
@@ -45,20 +90,27 @@ async function createOrder(status: OrderStatus) {
   }
 }
 
+// function saveOrder() {
+//   if (
+//     orderForm.userName &&
+//     orderForm.address &&
+//     orderForm.date &&
+//     orderForm.comment &&
+//     orderForm.product &&
+//     orderForm.status !== ''
+//   ) {
+//     const status: OrderStatus = orderForm.status
+//     createOrder(status)
+//   } else {
+//     console.log('Введите все данные')
+//   }
+// } Эта функция рабочая была до того, как сделали чтоб после нажатия на кнопку "Сохранить" под незаполненным полем выскакивала предупреждение.
+
 function saveOrder() {
-  if (
-    orderForm.userName &&
-    orderForm.address &&
-    orderForm.date &&
-    orderForm.comment &&
-    orderForm.product &&
-    orderForm.status !== ''
-  ) {
-    const status: OrderStatus = orderForm.status
-    createOrder(status)
-  } else {
-    console.log('Введите все данные')
+  if (!validateForm()) {
+    return
   }
+  createOrder(orderForm.status as OrderStatus)
 }
 
 function goToHomePage() {
@@ -74,47 +126,95 @@ function goToHomePage() {
       @submit.prevent="saveOrder"
     >
       <div class="form__row">
-        <BaseInput
-          id="user-name"
-          label="Имя заказчика"
-          v-model="orderForm.userName"
-          placeholder="Введите имя"
-        />
+        <div class="form__block">
+          <BaseInput
+            id="user-name"
+            label="Имя заказчика"
+            v-model="orderForm.userName"
+            placeholder="Введите имя"
+          />
+          <p
+            v-if="errors.userName"
+            class="error"
+          >
+            {{ errors.userName }}
+          </p>
+        </div>
 
-        <BaseInput
-          class="form__user-address"
-          id="address"
-          label="Адрес"
-          v-model="orderForm.address"
-          placeholder="Город, улица, дом, кв"
-        />
+        <div class="form__block">
+          <BaseInput
+            class="form__user-address"
+            id="address"
+            label="Адрес"
+            v-model="orderForm.address"
+            placeholder="Город, улица, дом, кв"
+          />
+          <p
+            v-if="errors.address"
+            class="error"
+          >
+            {{ errors.address }}
+          </p>
+        </div>
       </div>
 
       <div class="form__row">
-        <BaseInput
-          id="date"
-          type="date"
-          label="Дата"
-          v-model="orderForm.date"
-        />
+        <div class="form__block">
+          <BaseInput
+            id="date"
+            type="date"
+            label="Дата"
+            v-model="orderForm.date"
+          />
+          <p
+            v-if="errors.date"
+            class="error"
+          >
+            {{ errors.date }}
+          </p>
+        </div>
 
-        <OrderStatusSelect v-model="orderForm.status" />
+        <div class="form__block">
+          <OrderStatusSelect v-model="orderForm.status" />
+          <p
+            v-if="errors.status"
+            class="error"
+          >
+            {{ errors.status }}
+          </p>
+        </div>
       </div>
 
-      <BaseTextarea
-        label="Комментарий"
-        id="comment"
-        placeholder="Ваш комментарий"
-        v-model="orderForm.comment"
-      />
+      <div class="form__block">
+        <BaseTextarea
+          label="Комментарий"
+          id="comment"
+          placeholder="Ваш комментарий"
+          v-model="orderForm.comment"
+        />
+        <p
+          v-if="errors.comment"
+          class="error"
+        >
+          {{ errors.comment }}
+        </p>
+      </div>
 
-      <BaseInput
-        class="form__product"
-        id="product"
-        label="Название товара"
-        v-model="orderForm.product"
-        placeholder="Начните вводить название товара"
-      />
+      <div class="form__block">
+        <BaseInput
+          class="form__product"
+          id="product"
+          label="Название товара"
+          v-model="orderForm.product"
+          placeholder="Начните вводить название товара"
+        />
+        <p
+          v-if="errors.product"
+          class="error"
+        >
+          {{ errors.product }}
+        </p>
+      </div>
 
       <div class="form__buttons">
         <BaseButton
@@ -162,7 +262,20 @@ function goToHomePage() {
 
 .form__product {
   width: 100%;
-  margin-bottom: 40px;
+  margin-bottom: 20px;
+}
+
+.error {
+  max-width: 120px;
+  margin: 0 auto;
+  color: red;
+  font-size: 13px;
+  margin-bottom: 20px;
+}
+
+.form__block {
+  display: flex;
+  flex-direction: column;
 }
 </style>
 
